df2556f4961a567a8fcad5da8e3cc2aeee40065b
jmcelhaugh <tup59389@temple.edu>
2025-10-22 20:10:33 -0400

    Added Government Shutdown Banner
    Added a banner explaining the government shutdown to homepage.

--- exploring-national-parks/src/HomePage.js
+++ exploring-national-parks/src/HomePage.js
@@ -7,6 +7,7 @@
 import React from 'react'
 import Welcome from './HomePage/Components/Welcome'
 import Buttons from './HomePage/Components/Buttons'
+import Shutdown from './HomePage/Components/Shutdown'
 import yosemite from './HomePage/Assets/yosemite.jpg';
 import './Style/homepage.css'
 import HighlightGallery from './HomePage/Components/HighlightGallery';
@@ -15,7 +16,8 @@ const HomePage = () => {
     // <Navbar/>
     <div className = "home-page main-component">
         {/* <h1>Test Hello</h1> */}
-        <Welcome/>
+        <Shutdown/>
+	<Welcome/>
         <HighlightGallery/>
         <Buttons/>
     </div>
---
+++ exploring-national-parks/src/HomePage/Components/Shutdown.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+/**
+ * Component representing the shutdown section of the homepage.
+ * @module Shutdown
+ * @memberof HomePage
+ * @returns {JSX.Element} The rendered shutdown section.
+ */
+const Shutdown = () => {
+    return (
+        <div className = "shutdown">
+            <p className = "shutdown-text">
+                Attention - Due to the Government Shutdown, services at the National Parks may be limited. Please check official federal channels for more information.
+            </p>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Shutdown
